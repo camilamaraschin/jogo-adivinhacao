398636474bebb92438018d099cdaf107441f13d9
Camila Maraschin <105385268+camilamaraschin@users.noreply.github.com>
2022-12-30 06:46:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 
-const randomNumber = Math.round(Math.random()*10) // numeros aleatorios e inteiros
 let acertos = 1
+let randomNumber = Math.round(Math.random()*10) // numeros aleatorios e inteiros
 
 function handleTryClick(event){
     event.preventDefault()
-
     const inputNumber = document.querySelector("#inputNumber")
-
     if(Number(inputNumber.value) == randomNumber){
-       screen1.classList.add("hide") //esconder a primeira tela
+        screen1.classList.add("hide") //esconder a primeira tela
         screen2.classList.remove("hide") // aparecer a segunda tela
-
-        document.querySelector(".screen2 h2").innerText = ` acertou em ${acertos} tentativas` // mostrar o resultado
+        document.querySelector(".screen2 h2").innerText = ` Parabéns você acertou, o número escolhido é:  ${randomNumber}` // mostrar o resultado
+        randomNumber = Math.round(Math.random()*10) // numeros aleatorios e inteiros
+    }else{
+        acertos++
+        document.getElementById("tentenovamente").innerText = `tente novamente, você errou` // mostrar o resultado
     }
-
     inputNumber.value = ""
-acertos++
 }
 
 // fazer com que quando aparecer a segunda tela voltar para a primeira
